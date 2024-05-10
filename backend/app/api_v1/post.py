1e80d3c7848c609b2da9b19501586a728c98b32b
McMugah <blainelium@gmail.com>
2024-05-10 16:47:25 +0300

    updated posts routes

--- backend/app/api_v1/post.py
+++ backend/app/api_v1/post.py
@@ -1,6 +1,48 @@
+from flask import request
+from app.schema import PostSchema
+from app.model import Post
+from app import db
 from . import api
 
 
-@api.route('/')
-def get_home():
-    return "<h1>Karibu from Post</h1>"
+@api.route('/post', methods=['POST'])
+def new_post():
+    schema = PostSchema()
+    data = schema.load(request.json)
+    post = Post(**data)
+    db.session.add(post)
+    db.session.commit()
+    return {"message": "post created", "post": schema.dump(post)}, 201
+
+
+@api.route('/post', methods=['GET'])
+def get_posts():
+    schema = PostSchema(many=True)
+    posts = Post.query.all()
+    return {schema.dump(posts)}, 200
+
+
+@api.route('/post/<int:id>', methods=["GET"])
+def get_post(id):
+    schema = PostSchema()
+    post = Post.get_404(id)
+    return {schema.dump(post)}, 200
+
+
+@api.route('/post/<int:id>', methods=["PUT"])
+def edit_post(id):
+    schema = PostSchema()
+    post = Post.get_404(id)
+    db.session.add(post)
+    db.session.commit()
+    return {schema.dump(post)}, 201
+
+
+@api.route('/post/<int:id>', methods=["DELETE"])
+def delete_post(id):
+    schema = PostSchema()
+    post = Post.get_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return {"message": "post deleted"}, 200
+
